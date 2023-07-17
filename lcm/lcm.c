905abcf56b2e69c822a122152b1522c0069d0341
cariestevez <carinaestevezorth@gmail.com>
2023-07-17 21:29:25 +0200

    Add files via upload

---
+++ lcm/lcm.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned int    lcm(unsigned int a, unsigned int b)
+{
+	unsigned int	num;
+	
+	num = 0;
+	if (a == 0 || b == 0)
+		return (0);
+	if (a > b)
+		num = a;
+	else
+		num = b;
+	while (1)
+	{
+		if (num % a == 0 && num % b == 0)
+			return (num);
+		num++;
+	}
+}
